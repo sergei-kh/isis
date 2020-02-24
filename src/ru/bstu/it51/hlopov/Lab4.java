8b8f7312e0f2840174c1fc93f6fc14b04951502f
shlop <sergey.program.com@yandex.ru>
2020-02-24 11:24:34 +0300

    lab 4 file read scanner

--- src/ru/bstu/it51/hlopov/Lab4.java
+++ src/ru/bstu/it51/hlopov/Lab4.java
@@ -1,10 +1,21 @@
 package ru.bstu.it51.hlopov;
 import ru.bstu.it51.hlopov.helpers.regex.Email;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Lab4 {
     public void run() {
-        System.out.println("Введите путь до html файла: ");
-
-        Email regexEmail = new Email();
+        try {
+            Scanner inFile = new Scanner(new FileReader("files/main.html"));
+            Email email = new Email();
+            while (inFile.hasNextLine()) {
+                email.findEmailFromText(inFile.nextLine());
+            }
+            inFile.close();
+        } catch (IOException ex) {
+            System.out.println("Не могу открыть файл");
+        }
     }
 }
--- src/ru/bstu/it51/hlopov/helpers/regex/Email.java
+++ src/ru/bstu/it51/hlopov/helpers/regex/Email.java
@@ -2,18 +2,12 @@ package ru.bstu.it51.hlopov.helpers.regex;
 import java.util.regex.*;
 
 public class Email {
-    public Email() {
-        String str = "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает referent@vlgr.ranepa.ru" +
-                " Давно выяснено,@ lolol@ololo. .qewqe@qwe,ry 123@mail,ru rlro@mm,.ry что при оценке дизайна и композиции читаемый текст мешает prorector.nauka@vlgr.ranepa.ru Давно выяснено, " +
-                "что при оценке дизайна и композиции читаемый текст мешает grajd@vlgr.ranepa.ru что при оценке дизайна и композиции читаемый блабла@lolwqe skdjf @@@" +
-                "lol @ kwwj. ин фо@по чта.рф Fsk ueqwe rot ";
+    public void findEmailFromText(String text) {
         String regex = "([a-zа-яё0-9._-]+)(@{1})([a-zа-яё0-9._-]+)\\.([a-zа-я]{2})";
         Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
-        Matcher matchObj = pattern.matcher(str);
-        String match = "";
+        Matcher matchObj = pattern.matcher(text);
         while (matchObj.find()) {
-            match = matchObj.group();
-            System.out.println(match);
+            System.out.println(matchObj.group());
         }
     }
 }
